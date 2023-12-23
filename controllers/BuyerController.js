6f82fd25d03fbf9543fc2da83b25e5a4ee50ae4f
Abdullahtahirrr <abdullah.tahirr7@gmail.com>
2023-12-23 16:36:50 +0500

    added buyer, auth and seller controllers

---
+++ controllers/BuyerController.js
@@ -0,0 +1,26 @@
+class BuyerController {
+    async browseCompanies(req, res) {
+      // Logic for browsing companies by buyers
+    }
+  
+    async viewCart(req, res) {
+      // Logic for viewing cart by buyers
+    }
+  
+    async myActiveJobs(req, res) {
+      // Logic for viewing all orders by a buyer
+    }
+  
+    async viewOrderDetails(req, res) {
+      // Logic for viewing order details by a buyer
+    }
+  
+    async removeCartItem(req, res) {
+      // Logic for removing an item from the cart by a buyer
+    }
+  
+    // ... Other buyer-related methods
+  }
+  
+  module.exports = new BuyerController();
+  
---
+++ controllers/SellerController.js
@@ -0,0 +1,22 @@
+class SellerController {
+    async dashboardManageJobs(req, res) {
+      // Logic for managing jobs by sellers
+    }
+  
+    async updateProductPage(req, res) {
+      // Logic for opening the update product page by sellers
+    }
+  
+    async postProduct(req, res) {
+      // Logic for posting a product by sellers
+    }
+  
+    async companyProfile(req, res) {
+      // Logic for viewing a company profile by sellers
+    }
+  
+    // ... Other seller-related methods
+  }
+  
+  module.exports = new SellerController();
+  
